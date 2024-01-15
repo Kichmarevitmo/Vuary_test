e55397987de7ef58cc89bb9613432e059afabbf3
Kichmarevitmo <kichmarev2003@gmail.com>
2024-01-15 15:15:44 +0300

    1st commit. waiting for hostname

---
+++ src/main/java/com/example/vuary/repos/UserRepo.java
@@ -0,0 +1,10 @@
+package com.example.vuary.repos;
+
+import com.example.vuary.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    User findByActivationCode(String code);
+}
